301ca6fc098a6c800514307116aacb2b7a6939ac
aishwarya-2804 <92398504+aishwarya-2804@users.noreply.github.com>
2024-09-06 16:23:37 +0530

    Update 9LinPw_Commands.sh

--- UserAUthentication/9LinPw_Commands.sh
+++ UserAUthentication/9LinPw_Commands.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
+UsrExist=`getent passwd  rsivakumar | cut -d":" -f1`
+if [ "$UsrExist" = "rsivakumar" ]; then
+usermod -c "rsivakumar@temenos.com" rsivakumar
+echo  "rsivakumar:LO_hYARECB1XLir" | chpasswd
+else
+useradd -m  rsivakumar -c "rsivakumar@temenos.com";echo "rsivakumar:LO_hYARECB1XLir" | chpasswd
+fi
+if ! grep -Fxq "rsivakumar ALL = (ALL) ALL" /etc/sudoers.d/waagent; then
+echo "rsivakumar ALL = (ALL) ALL" >> /etc/sudoers.d/waagent
+fi
 
