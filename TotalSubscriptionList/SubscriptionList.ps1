1aeb990261f8aebdb043ac0bed0d62f1f7b71d0f
aishwarya-2804 <92398504+aishwarya-2804@users.noreply.github.com>
2024-09-25 16:11:13 +0530

    Update SubscriptionList.ps1

--- TotalSubscriptionList/SubscriptionList.ps1
+++ TotalSubscriptionList/SubscriptionList.ps1
@@ -1,1 +1,36 @@
+# Connect to Azure
+Connect-AzAccount
 
+# Get all subscriptions
+$subscriptions = Get-AzSubscription
+
+# Create an array to store the subscription information
+$subscriptionInfo = @()
+
+# Loop through each subscription
+foreach ($subscription in $subscriptions) {
+    # Retrieve the subscription details
+    $subscriptionName = $subscription.Name
+    $subscriptionId = $subscription.Id
+
+    # Create a custom object with the subscription information
+    $subscriptionObject = [PSCustomObject]@{
+        'Subscription Name' = $subscriptionName
+        'Subscription ID' = $subscriptionId
+    }
+
+    # Add the subscription object to the array
+    $subscriptionInfo += $subscriptionObject
+}
+
+# Get today's date in "yyyy-MM-dd" format
+$date = Get-Date -Format "yyyy-MM-dd"
+
+# Define the export path with the current date in the file name
+$exportPath = "C:\path\to\file\SubscriptionInfo_$date.csv"
+
+# Export the subscription information to a CSV file
+$subscriptionInfo | Export-Csv -Path $exportPath -NoTypeInformation
+
+# Display the export path
+Write-Host "Subscription information exported to: $exportPath"
